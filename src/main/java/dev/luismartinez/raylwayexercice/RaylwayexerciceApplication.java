251634b7c86761591893f93db3842ea5ff6801d9
unknown <luiscarlos81@gmail.com>
2024-02-04 21:40:18 -0600

    controlador

---
+++ src/main/java/dev/luismartinez/raylwayexercice/RaylwayexerciceApplication.java
@@ -0,0 +1,13 @@
+package dev.luismartinez.raylwayexercice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaylwayexerciceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaylwayexerciceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/luismartinez/raylwayexercice/RaylwayexerciceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luismartinez.raylwayexercice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaylwayexerciceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
